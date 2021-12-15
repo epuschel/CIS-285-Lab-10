8635efe2b0d036082267cf067960aaf56739b45d
epuschel <73982403+epuschel@users.noreply.github.com>
2021-12-14 21:09:23 -0500

    Fixed SelectionSort.java

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,32 +1,28 @@
 public class SelectionSort {
-	//public class SelectionSort {
-		 private int temp;
+	 private int temp;
 
-		/** Creates a new instance of SelectionSort */
-	    public SelectionSort() {
-	    }
-	    
-	    /* A simple SelectionSort algorithm
-	     * pre-condition: An unsorted array
-	     * post-condition: A sorted array
-	     * inputs: An array
-	     * outputs: An array
-	     * special conditions:
-	     */    
-	    public int[] basicSelectionSort(int[] x) {
-	        for (int i = 0; i < x.length; ++i) {
-	            for (int j= 0; j < x.length; ++j) {
-	            	if (i != j) {
-	            		if (x[i] < x[j]) {
-	            			temp = x[j];
-	            			x[j] = x[i];
-	            			x[i] = temp;
-	                }
-	            	}
-	            } // end of inner for loop
-	        } // end of outer for loop
-	        return x;
-	    } // end of basicSelectionSort method
+	/** Creates a new instance of SelectionSort */
+    public SelectionSort() {
+    }
+    
+    /* A simple SelectionSort algorithm
+     * pre-condition: 
+     * post-condition:
+     * inputs:
+     * outputs:
+     * special conditions:
+     */    
+    public int[] basicSelectionSort(int[] x) {
+        for (int i = 0; i < x.length; ++i) {
+            for (int j= i+1; j < x.length; ++j) {
+                if (x[i] > x[j]) {
+                    temp = x[i];
+                    x[i] = x[j];
+                    temp = x[j];
+                }
+            } // end of inner for loop
+        } // end of outer for loop
+        return x;
+    } // end of basicSelectionSort method
 
-	//}
 }
